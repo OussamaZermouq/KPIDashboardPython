d439a211f35f86cce9b01468c25cf7f4a3a6945e
OussamaZermouq <oussamazermouq2@gmail.com>
2025-04-24 11:11:33 +0100

    Patched some code and added a new endpoint

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 import pandas as pd
 import io
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+origins= [
+    "http://localhost:3000"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...), sheet_name: str = "Hourly Ville"):
     # Checking if the file is an Excel sheet
@@ -26,12 +37,9 @@ async def upload_file(file: UploadFile = File(...), sheet_name: str = "Hourly Vi
                 detail=f"Sheet '{sheet_name}' not found. Available sheets: {xl.sheet_names}"
             )
         
-        # Read the specific sheet with header row (assumed to be row 1)
-        data = pd.read_excel(io.BytesIO(content), engine="openpyxl", sheet_name=sheet_name, header=1)
+        data = pd.read_excel(io.BytesIO(content), engine="openpyxl", sheet_name=sheet_name, header=1, index_col=0)
         logging.info(f"Data Preview:\n{data.head()}")
 
-        # Rename columns: If column name is NaN or starts with "Unnamed"
-        data.columns = [f"Column_{i}" if pd.isna(col) or str(col).startswith("Unnamed") else col for i, col in enumerate(data.columns)]
         
         # Fill NaN and replace infinite values
         data = data.fillna("").replace([float("inf"), float("-inf")], 0)
@@ -41,3 +49,20 @@ async def upload_file(file: UploadFile = File(...), sheet_name: str = "Hourly Vi
     except Exception as e:
         logging.error(f"Error processing file: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+@app.post("/sheets")
+async def getSheetNames(file: UploadFile = File(...)):
+    if not file.filename.endswith(".xlsx"):
+        raise HTTPException(status_code=400, detail="File must be an Excel sheet")
+    
+    try:
+        content = await file.read()
+        xl = pd.ExcelFile(io.BytesIO(content), engine="openpyxl")
+        logging.info(f"Sheet Names: {xl.sheet_names}")
+        return {"data": xl.sheet_names}
+
+    except Exception as e:
+        logging.error("Error processing file")
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+        
+    
